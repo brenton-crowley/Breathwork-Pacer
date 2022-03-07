7c11e9c9e08d66a9523a518dc0052e2de8fb6550
Brent <brenton.crowley@gmail.com>
2022-03-07 16:41:31 +1100

    Initial Commit

---
+++ Breathwork Pacer/Breathwork_PacerApp.swift
@@ -0,0 +1,20 @@
+//
+//  Breathwork_PacerApp.swift
+//  Breathwork Pacer
+//
+//  Created by Brent on 7/3/2022.
+//
+
+import SwiftUI
+
+@main
+struct Breathwork_PacerApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
